dcfd78b42933d513d14879c706c02fa43f24d6db
Constantin Roganov <rccbox@gmail.com>
2016-09-22 21:31:58 +0300

    Added package configuration files

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+  name = 'peewee_mssql',
+  packages = [],
+  version = '0.1',
+  description = 'MS SQL Server support for the peewee ORM',
+  author = 'Constantin Roganov',
+  author_email = 'rccbox@gmail.com',
+  url = 'https://github.com/brake/peewee_mssql',
+  download_url = 'https://github.com/brake/peewee_mssql/archive/0.1.0.zip', 
+  keywords = ['database', 'ORM', 'peewee', 'mssql'],
+  classifiers = [],
+)
